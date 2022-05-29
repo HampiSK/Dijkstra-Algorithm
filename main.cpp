4bb523b89f05e84da43e4ab8c407b805eff80408
HampiSK <58185925+HampiSK@users.noreply.github.com>
2022-05-29 23:10:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,43 @@
 #include "pch.hpp"
 
-int main()
+int main(void)
 {
-    Graph GraphD; // Create Graph with empty matrix
+    Matrix matrix; // Create matrix
 
     // Insert vertices
-    // Note: Vertices are shown as chars, but in graphD are converted into unsigned int
-    GraphD.Add('A');
-    GraphD.Add('B');
-    GraphD.Add('C');
-    GraphD.Add('D');
-    GraphD.Add('E');
-    GraphD.Add('F');
-    GraphD.Add('T');
-    GraphD.Add('O');
+    matrix.Add('A');
+    matrix.Add('B');
+    matrix.Add('C');
+    matrix.Add('D');
+    matrix.Add('E');
+    matrix.Add('F');
+    matrix.Add('T');
+    matrix.Add('O');
 
     // Connect with edges and weight
-    GraphD.Add('O', 'A', 2);
-    GraphD.Add('O', 'B', 5);
-    GraphD.Add('O', 'C', 4);
-    GraphD.Add('A', 'B', 2);
-    GraphD.Add('A', 'D', 7);
-    GraphD.Add('A', 'F', 12);
-    GraphD.Add('B', 'C', 1);
-    GraphD.Add('B', 'D', 4);
-    GraphD.Add('B', 'E', 3);
-    GraphD.Add('C', 'E', 4);
-    GraphD.Add('D', 'E', 1);
-    GraphD.Add('D', 'T', 5);
-    GraphD.Add('E', 'T', 7);
-    GraphD.Add('F', 'T', 3);
+    matrix.Add('O', 'A', 2);
+    matrix.Add('O', 'B', 5);
+    matrix.Add('O', 'C', 4);
+    matrix.Add('A', 'B', 2);
+    matrix.Add('A', 'D', 7);
+    matrix.Add('A', 'F', 12);
+    matrix.Add('B', 'C', 1);
+    matrix.Add('B', 'D', 4);
+    matrix.Add('B', 'E', 3);
+    matrix.Add('C', 'E', 4);
+    matrix.Add('D', 'E', 1);
+    matrix.Add('D', 'T', 5);
+    matrix.Add('E', 'T', 7);
+    matrix.Add('F', 'T', 3);
     
-    GraphD.Print();
+    matrix.PrintMatrix();
+    matrix.PrintWeights();
     // Shortest path from end to start
-    const auto PATH = GraphD.ShortestPath('O','T');
+    const auto PATH = matrix.ShortestPath('O','T');
 
     std::cout<< "Shortest Path between 'O' node and 'T' node:\n";
     // Display shortest path from start to end
     for(int i = PATH.size() - 1; i >= 0; i--)
-        std::cout<<(char)PATH[i]<<" "; // unsigned int are displayed as chars
+        std::cout<<(char)PATH[i]<<' '; 
+        
     std::cout<<std::endl; // Flush
-}
